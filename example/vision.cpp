60e313f4373e0ac3cb00d142fd2c672a83726964
sin0emp <sina.11235@gmail.com>
2014-12-02 12:55:47 +0330

    * vision example modified. It has 75% linearly separable performance for the face-nonface task. More complex classifiers (MLP tested) gaind 100% performance.

--- example/vision.cpp
+++ example/vision.cpp
@@ -5,10 +5,15 @@ void firstLayer()
 {
    VisualNetwork visNet;
    visNet.setDefaultSTDPParameters(0.99f, 0.002f, 0.002f, 1.0f, 100, 20.0f, 30.0f);
-   visNet.setDefaultBoundingParameters(2.0f, 30, 0.5f, 1.5f, 15, 15, 1, 5);
-   int input = visNet.addLayer<IzhikevichNeuron>(Point2D(142, 142), INPUT_LAYER);
-   visNet.setInputImagesDirectory("C:/Pix/filtered 2class/");
-   int s1 = visNet.addReceptiveFieldSuperLayer<IzhikevichNeuron>(input, 6, Point2D(10,10), Point2D(4,4), new IzhikevichParameters(0.02, 0.2, -65, 6));
+   int input = visNet.addLayer<IzhikevichNeuron>(Point2D(120, 120), INPUT_LAYER);
+   visNet.setInputImagesDirectory("C:/Pix/101Object-edited/resize/");
+
+   visNet.setDefaultBoundingParameters(1.7f, 30, 0.0f, 1.0f, 15, 15, 1, 5);
+   int s1 = visNet.addSimpleCellSuperLayer<IzhikevichNeuron>(input, 6, Point2D(10,10), Point2D(4,4), new IzhikevichParameters(0.02, 0.2, -65, 6));
+
+   visNet.setDefaultBoundingParameters(20.0f, 0, 20.0f, 20.0f, 0, 0, 0, 0);
+   int c1 = visNet.addComplexCellFromSuperLayer<IzhikevichNeuron>(s1, Point2D(3, 3), Point2D(1, 1), new IzhikevichParameters(0.02, 0.2, -65, 6));
+
    visNet.runNetwork(EPOCH_NUMBER, 10);
    VisualNetwork::saveNetwork(visNet, "SL1.sav");
 }
@@ -17,8 +22,13 @@ void secondLayer()
 {
    VisualNetwork* visNet = VisualNetwork::loadNetwork("SL1.sav");
    visNet->setLearningLock(true);
-   visNet->setDefaultBoundingParameters(3.0f, 30, 1.5f, 2.5f, 15, 15, 1, 5);
-   int s2 = visNet->addReceptiveFieldFromSuperLayer<IzhikevichNeuron>(0, 6, Point2D(3, 3), Point2D(2, 2));
+
+   visNet->setDefaultBoundingParameters(1.5f, 30, 0.2f, 1.2f, 15, 15, 1, 5);
+   int s2 = visNet->addSimpleCellFromSuperLayer<IzhikevichNeuron>(1, 6, Point2D(3, 3), Point2D(2, 2));
+
+   visNet->setDefaultBoundingParameters(20.0f, 0, 20.0f, 20.0f, 0, 0, 0, 0);
+   int c2 = visNet->addComplexCellFromSuperLayer<IzhikevichNeuron>(s2, Point2D(3, 3), Point2D(1, 1), new IzhikevichParameters(0.02, 0.2, -65, 6));
+
    visNet->runNetwork(EPOCH_NUMBER, 10);
    VisualNetwork::saveNetwork(*visNet, "SL2.sav");
    delete visNet;
@@ -28,8 +38,13 @@ void thirdLayer()
 {
    VisualNetwork* visNet = VisualNetwork::loadNetwork("SL2.sav");
    visNet->setLearningLock(true);
-   visNet->setDefaultBoundingParameters(4.0f, 30, 3.0f, 4.0f, 15, 15, 1, 10);
-   int s3 = visNet->addReceptiveFieldFromSuperLayer<IzhikevichNeuron>(1, 6, Point2D(4, 4), Point2D(2, 2));
+
+   visNet->setDefaultBoundingParameters(1.5f, 30, 0.2f, 1.2f, 15, 15, 1, 10);
+   int s3 = visNet->addSimpleCellFromSuperLayer<IzhikevichNeuron>(3, 6, Point2D(4, 4), Point2D(2, 2));
+
+   visNet->setDefaultBoundingParameters(20.0f, 0, 20.0f, 20.0f, 0, 0, 0, 0);
+   int c3 = visNet->addComplexCellFromSuperLayer<IzhikevichNeuron>(s3, Point2D(3, 3), Point2D(1, 1), new IzhikevichParameters(0.02, 0.2, -65, 6));
+
    visNet->runNetwork(EPOCH_NUMBER, 10);
    VisualNetwork::saveNetwork(*visNet, "SL3.sav");
    delete visNet;
@@ -39,8 +54,10 @@ void fourthLayer()
 {
    VisualNetwork* visNet = VisualNetwork::loadNetwork("SL3.sav");
    visNet->setLearningLock(true);
-   visNet->setDefaultBoundingParameters(4.0, 30, 3.0f, 4.0f, 15, 15, 1, 10);
-   int s3 = visNet->addReceptiveFieldFromSuperLayer<IzhikevichNeuron>(2, 6, Point2D(5, 5), Point2D(2, 2));
+
+   visNet->setDefaultBoundingParameters(1.5f, 30, 0.2f, 1.2f, 15, 15, 1, 10);
+   int s3 = visNet->addSimpleCellFromSuperLayer<IzhikevichNeuron>(5, 6, Point2D(5, 5), Point2D(2, 2));
+
    visNet->runNetwork(EPOCH_NUMBER, 10);
    VisualNetwork::saveNetwork(*visNet, "SL4.sav");
    delete visNet;
@@ -48,36 +65,10 @@ void fourthLayer()
 
 void test()
 {
-   VisualNetwork* visNet = VisualNetwork::loadNetwork("SL4.sav");
+   VisualNetwork* visNet = VisualNetwork::loadNetwork("SL3.sav");
    visNet->setLearningLock(true);
 
-   //visNet->logLayerActivity(1);
-   //visNet->logLayerActivity(2);
-   //visNet->logLayerActivity(3);
-   //visNet->logLayerActivity(4);
-   //visNet->logLayerActivity(5);
-   //visNet->logLayerActivity(6);
-
-   //visNet->logLayerActivity(7);
-   //visNet->logLayerActivity(8);
-   //visNet->logLayerActivity(9);
-   //visNet->logLayerActivity(10);
-   //visNet->logLayerActivity(11);
-   //visNet->logLayerActivity(12);
-
-   //visNet->logLayerActivity(13);
-   //visNet->logLayerActivity(14);
-   //visNet->logLayerActivity(15);
-   //visNet->logLayerActivity(16);
-   //visNet->logLayerActivity(17);
-   //visNet->logLayerActivity(18);
-
-   visNet->logLayerActivity(19);
-   visNet->logLayerActivity(20);
-   visNet->logLayerActivity(21);
-   visNet->logLayerActivity(22);
-   visNet->logLayerActivity(23);
-   visNet->logLayerActivity(24);
+   visNet->logSuperLayerActivity(5);
 
    visNet->runNetwork(EPOCH_NUMBER, 1);
    delete visNet;
@@ -88,7 +79,7 @@ int main()
    //firstLayer();
    //secondLayer();
    //thirdLayer();
-   fourthLayer();
+   //fourthLayer();
    test();
    
    return 0;
